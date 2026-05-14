c23ea955660b0e0679130f2831a42e0adb532006
Karolina Krysiak <71433414+KarolinaKrysiak@users.noreply.github.com>
2026-05-14 12:59:15 +0200

    quit button functionality

--- game/Timeless/Assets/Scripts/MainMenu.cs
+++ game/Timeless/Assets/Scripts/MainMenu.cs
@@ -1,4 +1,3 @@
-using System.ComponentModel.Design;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -7,5 +6,11 @@ public class MainMenu : MonoBehaviour
     public void PlayGame ()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void QuitGame ()
+    {
+        Debug.Log ("Quit game");
+        Application.Quit();
     }
 }
