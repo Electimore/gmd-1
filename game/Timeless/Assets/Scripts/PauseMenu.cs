e8c5a82456e4dde5cd5760a033de5797254cb5c0
Karolina Krysiak <karolinakrysiak01@gmail.com>
2026-05-19 22:00:01 +0200

    base functionality for pause menu

--- game/Timeless/Assets/Scripts/PauseMenu.cs
+++ game/Timeless/Assets/Scripts/PauseMenu.cs
@@ -1,16 +1,58 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
 
 public class PauseMenu : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public static bool GameIsPaused = false;
+    public GameObject PauseMenuUI;
+
+    private void Start()
     {
-        
+        PauseMenuUI.SetActive(false);
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (Keyboard.current.escapeKey.wasPressedThisFrame)
+        {
+            if (GameIsPaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+
+    public void Resume()
+    {
+        PauseMenuUI.SetActive(false);
+        Cursor.lockState = CursorLockMode.Locked;
+        Time.timeScale = 1f;
+        GameIsPaused = false;
+    }
+
+    void Pause()
+    {
+        PauseMenuUI.SetActive(true);
+        Cursor.lockState = CursorLockMode.None;
+        Time.timeScale = 0f;
+        GameIsPaused = true;
+    }
+
+    public void LoadMenu()
+    {
+        Debug.Log("Loading main menu...");
+        GameIsPaused = false;
+        SceneManager.LoadScene(0);
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Quitting game...");
+        Application.Quit();
     }
 }
